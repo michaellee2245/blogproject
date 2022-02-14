1fe023bfc172424460ae2072b79821f42eb8bfd1
Michael Lee <michaellee2245@gmail.com>
2022-02-14 15:20:50 -0700

    initial commit

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIHdaeBXF5KWK9wCYqGAYRqcKEAlkWxJI",
+  authDomain: "blogproject-b264c.firebaseapp.com",
+  projectId: "blogproject-b264c",
+  storageBucket: "blogproject-b264c.appspot.com",
+  messagingSenderId: "409682803972",
+  appId: "1:409682803972:web:197cc4951884795e132451"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
